9090415630706744b4a755469563ad01bb989dd5
sshhery <shery.h233@gmail.com>
2023-03-17 17:50:55 -0300

    re intentando

--- src/main/java/com/portfolio/mgb/Controller/CExperiencia.java
+++ src/main/java/com/portfolio/mgb/Controller/CExperiencia.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/explab")
-@CrossOrigin(origins = {"http://localhost:4200"})
+@CrossOrigin(origins = "http://localhost:4200")
 public class CExperiencia {
     @Autowired
     SExperiencia sExperiencia;
