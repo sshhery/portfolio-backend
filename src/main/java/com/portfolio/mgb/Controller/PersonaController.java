f4978714da9caabcfebecf2eacf1bbbaf1ba2dcb
sshhery <shery.h233@gmail.com>
2023-03-22 22:27:49 -0300

    comios en crear

--- src/main/java/com/portfolio/mgb/Controller/PersonaController.java
+++ src/main/java/com/portfolio/mgb/Controller/PersonaController.java
@@ -9,13 +9,17 @@ import com.portfolio.mgb.Entity.Persona;
 import com.portfolio.mgb.Security.Controller.Mensaje;
 import com.portfolio.mgb.Service.ImpPersonaService;
 import java.util.List;
+import java.util.Optional;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,7 +37,35 @@ public class PersonaController {
     public ResponseEntity<List<Persona>> list(){
         List<Persona> list = personaService.list();
         return new ResponseEntity(list, HttpStatus.OK);
+        
     }
+    
+    
+    
+      @GetMapping("/traer")
+    public List<Persona> getPersona(){
+        return personaService.list();
+    }
+    @GetMapping("/traer/perfil")
+    public Optional<Persona> findPersona(){
+        return personaService.getOne((int) 1);
+    }
+
+    
+    
+    @PostMapping("/crear")
+    public String createPersona(@RequestBody Persona persona){
+        personaService.save(persona);
+        return "La persona fue creada correctamente";
+    }
+    
+    @PreAuthorize("hasRole('ADMIN')")
+    @DeleteMapping("/borrar/{id}")
+    public String deletePersona(@PathVariable int id){
+        personaService.delete(id);
+        return "La persona fue eliminada corredctamente";
+    }
+    
     
     @GetMapping("/detail/{id}")
     public ResponseEntity<Persona> getById(@PathVariable("id")int id){
